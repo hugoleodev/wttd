db9401f92b959aee456a8446c1f3819031d77bca
Hugo Leonardo C. Silva <hugoleodev@gmail.com>
2013-08-11 00:34:14 -0300

    Termino da etapa 3

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -33,10 +33,20 @@ class SubscriptionFormTest(TestCase):
         'Email is optional.'
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
+        
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized'
+        form = self.make_validated_form(name='HUGO leonardo')
+        self.assertEqual('Hugo Leonardo', form.cleaned_data['name'])
+        
+    def test_must_inform_email_or_phone(self):
+        'Email or phone are optional, but one must be informed'
+        form = self.make_validated_form(email='', phone_0='', phone_1='')
+        self.assertItemsEqual(['__all__'], form.errors)
     
     def make_validated_form(self, **kwargs):
         data = dict(name='Hugo Leonardo', email='hugoleodev@gmail.com',
-                    cpf='12345678901', phone='32-91386511')
+                    cpf='12345678901', phone_0='32', phone_1='91386511')
         data.update(kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
--- eventex/subscriptions/tests/test_views_subscribe.py
+++ eventex/subscriptions/tests/test_views_subscribe.py
@@ -20,8 +20,8 @@ class SubscribeTest(TestCase):
     def test_html(self):
         'Html must contain input controls'
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 5)
-        self.assertContains(self.resp, 'type="text"', 4)
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 5)
         self.assertContains(self.resp, '<button type="submit"')
 
     def test_csrf(self):
@@ -68,3 +68,11 @@ class SubscribeInvalidPostTest(TestCase):
     def test_dont_save(self):
         'Do not save data'
         self.assertFalse(Subscription.objects.exists())
+        
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        'Check if non_field_errors are shown in template.'
+        invalid_data = dict(name='Hugo Leonardo', cpf='00000000001')
+        response = self.client.post(r('subscriptions:subscribe'), invalid_data)
+        
+        self.assertContains(response, '<ul class="errorlist">')
